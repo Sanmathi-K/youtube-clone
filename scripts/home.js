a490448e7789f0a314707d9b07ed1afc7faae6eb
Sanmathi-k <sanmathikannapiran@gmail.com>
2022-02-08 23:47:59 +0530

    .

--- scripts/home.js
+++ scripts/home.js
@@ -1,104 +1,102 @@
 function setNewImage1()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
     
     document.getElementById("img1").src="https://i.ytimg.com/an_webp/_uQrJ0TkZlc/mqdefault_6s.webp?du=3000&sqp=CI_miZAG&rs=AOn4CLCK3xub1OlIQDFI7q_jSIhscDPh7g";
    
 }
 function setOldImage1()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img1").src="https://cdn.educba.com/academy/wp-content/uploads/2016/03/Python-Programming-for-the-Absolute-Beginner.jpg";
     
 }
 function setNewImage2()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
     
     document.getElementById("img2").src="https://i.ytimg.com/an_webp/W6NZfCO5SIk/mqdefault_6s.webp?du=3000&sqp=CJbUiZAG&rs=AOn4CLA-uIYYtn6cI5_pX6Byglhj5X4L4g";
    
 }
 function setOldImage2()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img2").src="https://www.game-designers.net/wp-content/uploads/2019/12/JavaScript-Tutorial-for-Beginners-Learn-JavaScript-in-1-Hour-2019.jpg";
     
 }
 function setNewImage3()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
-    
+
     document.getElementById("img3").src="https://i.ytimg.com/an_webp/eIrMbAQSU34/mqdefault_6s.webp?du=3000&sqp=CJb1iZAG&rs=AOn4CLDatgWEjVJ9k7BCLxGwUlg0oLG9PA";
    
 }
 function setOldImage3()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img3").src="https://i.ytimg.com/vi/eIrMbAQSU34/maxresdefault.jpg";
     
 }
 function setNewImage4()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
     
-    document.getElementById("img4").src="https://i.ytimg.com/an_webp/Ke90Tje7VS0/mqdefault_6s.webp?du=3000&sqp=CNGVipAG&rs=AOn4CLAJTKv85kgQgwvzZsDXgwGjhsGdcg";
+     document.getElementById("img4").src="https://i.ytimg.com/an_webp/Ke90Tje7VS0/mqdefault_6s.webp?du=3000&sqp=CNGVipAG&rs=AOn4CLAJTKv85kgQgwvzZsDXgwGjhsGdcg";
    
 }
 function setOldImage4()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img4").src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN";
     
 }
 function setNewImage5()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
     
     document.getElementById("img5").src="https://i.ytimg.com/an_webp/i8xsbYgMiBs/mqdefault_6s.webp?du=3000&sqp=CPmlipAG&rs=AOn4CLBbSPjn0uoz36Ha5DxtfyCwTJuBaQ";
    
 }
 function setOldImage5()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img5").src="https://ourcodeworld.com/public-media/articles/articleocw-5d653602443b7.webp";
     
 }
 function setNewImage6()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+   
     
     document.getElementById("img6").src="https://i.ytimg.com/an_webp/8JJ101D3knE/mqdefault_6s.webp?du=3000&sqp=CPOvipAG&rs=AOn4CLBWFHqhi45sExuKL4cmiBmO6LjDIg";
    
 }
 function setOldImage6()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img6").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKX2rc3-Um-Vw0kH9gEDe35s8C61TwXBTvMw&usqp=CAU";
     
 }
 function setNewImage7()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
     
     document.getElementById("img7").src="https://i.ytimg.com/an_webp/k5E2AVpwsko/mqdefault_6s.webp?du=3000&sqp=CI6BipAG&rs=AOn4CLBPvYzMRFxNq5ykmtJNmWFn5cwQXA";
    
 }
 function setOldImage7()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img7").src="https://i.morioh.com/2020/02/03/32bd329b87ed.jpg";
     
 }
 function setNewImage8()
 {
-    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
     
     document.getElementById("img8").src="https://i.ytimg.com/an_webp/88PXJAA6szs/mqdefault_6s.webp?du=3000&sqp=CLW8ipAG&rs=AOn4CLA-kCa_cXa--4rSUDO43HyO9H5DQw";
    
 }
 function setOldImage8()
 {
-    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    
     document.getElementById("img8").src="https://www.hostingclues.com/wp-content/uploads/Learning-HTML-for-Beginners-Banners.png";
     
 }
